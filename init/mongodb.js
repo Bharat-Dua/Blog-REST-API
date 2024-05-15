51798a30acf5b9a95f3b5aac302b86fa6f07b959
Bharat Dua <bharatdua13@gmail.com>
2024-05-15 13:06:58 +0530

    setup the user schema

--- init/mongodb.js
+++ init/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const {connectionUrl} = require("../config/kyes");
+const { connectionUrl } = require("../config/kyes");
 const connectMongodb = async () => {
   try {
     await mongoose.connect(connectionUrl);
