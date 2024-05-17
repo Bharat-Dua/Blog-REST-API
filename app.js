95fc9c1e43b819c27bacc63dd83b08c448938a89
Bharat Dua <bharatdua13@gmail.com>
2024-05-17 11:04:17 +0530

    added morgan package to see req detail

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotEnv = require("dotenv");
+const morgan = require("morgan")
 dotEnv.config();
 
 const connectMongodb = require("./init/mongodb");
@@ -13,6 +14,9 @@ const app = express();
 connectMongodb();
 app.use(express.json({ limit: "500mb" }));
 app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
-app.use("/api/v1/auth",authRoute)
+app.use(morgan("dev"))
+
+// route section
+app.use("/api/v1/auth",authRoute);
 
 module.exports = app;
