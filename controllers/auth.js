1ce78d57c5aa0bb3bed862840c08c18f676b52ae
Bharat Dua <bharatdua13@gmail.com>
2024-05-19 13:43:38 +0530

    user signup validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,34 @@ const { User } = require("../models");
 const signup = async (req, res, next) => {
   try {
     const { name, email, password, role } = req.body;
+
+    // signup validation
+    // if (!name) {
+    //   res.code = 400;
+    //   throw new Error("Name is required");
+    // }
+
+    // if (!email) {
+    //   res.code = 400;
+    //   throw new Error("Email is required");
+    // }
+
+    // if (!password) {
+    //   res.code = 400;
+    //   throw new Error("Password is required");
+    // }
+
+    // if (password.length < 6) {
+    //   res.code = 400;
+    //   throw new Error("Password must be at least 6 characters");
+    // }
+
+    const isEmailExits = await User.findOne({ email });
+    if (isEmailExits) {
+      res.code = 400;
+      throw new Error("Email already exist");
+    }
+
     const newUser = new User({ name, email, password, role });
     await newUser.save();
     res.status(201).json({
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { authController } = require("../controllers");
-router.post("/signup", authController.signup);
+const { signupValidate } = require("../validator/auth");
+const validate = require("../validator/validate");
+router.post("/signup", signupValidate, validate, authController.signup);
 module.exports = router;
