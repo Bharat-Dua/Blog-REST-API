d004520f06d4454eb94f01bc133e79adfd90c4a4
Bharat Dua <bharatdua13@gmail.com>
2024-05-20 16:19:22 +0530

    hashed password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { User } = require("../models");
+const hashPassword = require("../utils/hashPassword");
 const signup = async (req, res, next) => {
   try {
     const { name, email, password, role } = req.body;
@@ -23,14 +24,19 @@ const signup = async (req, res, next) => {
     //   res.code = 400;
     //   throw new Error("Password must be at least 6 characters");
     // }
-
+    const hashPasswordValue = await hashPassword(password);
     const isEmailExits = await User.findOne({ email });
     if (isEmailExits) {
       res.code = 400;
       throw new Error("Email already exist");
     }
 
-    const newUser = new User({ name, email, password, role });
+    const newUser = new User({
+      name,
+      email,
+      password: hashPasswordValue,
+      role,
+    });
     await newUser.save();
     res.status(201).json({
       code: 201,
